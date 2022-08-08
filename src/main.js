3f7968e1742292f58667aab45b3069daa4f093f8
unknown <sylvanobernardinv@gmail.com>
2022-08-08 20:44:59 +0300

    e_Fako 0.1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faCircleUser, faUser, faKey)
 
 
-createApp(App).mount('#app')
+import './assets/main.css'
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
